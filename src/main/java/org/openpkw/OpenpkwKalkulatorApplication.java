5460ad09a662a1ddb038598bb2375b2fa22e4f1d
Adam Chyła <adam@chyla.org>
2016-03-18 19:33:22 +0100

    Initial project files

---
+++ src/main/java/org/openpkw/OpenpkwKalkulatorApplication.java
@@ -0,0 +1,12 @@
+package org.openpkw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenpkwKalkulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenpkwKalkulatorApplication.class, args);
+	}
+}
