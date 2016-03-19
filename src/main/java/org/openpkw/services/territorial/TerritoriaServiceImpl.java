a3a991d17275803c4675ccafff8224a5e327e5d0
mrozi.m@gmail.com <marlena56>
2016-03-19 03:42:27 +0100

    Init controller.

--- src/main/java/org/openpkw/services/territorial/TerritoriaServiceImpl.java
+++ src/main/java/org/openpkw/services/territorial/TerritoriaServiceImpl.java
@@ -1,5 +1,6 @@
 package org.openpkw.services.territorial;
 
+import org.openpkw.model.entity.Province;
 import org.openpkw.model.repositories.CommunityRepository;
 import org.openpkw.model.repositories.CountyRepository;
 import org.openpkw.model.repositories.ProvinceRepository;
@@ -11,6 +12,9 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+
+import java.util.ArrayList;
+
 /**
  * Created by mrozi on 3/19/2016.
  */
@@ -28,9 +32,11 @@ public class TerritoriaServiceImpl implements TerritorialService{
     @Autowired
     private ProvinceRepository provinceRepository;
 
+
+
     @Override
     public void initTerritorialService() throws Exception {
-         this.getClass().getResourceAsStream(territorialFile);
+        ArrayList<Province> provinceList = new ArrayList<Province>();
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse( this.getClass().getResourceAsStream(territorialFile));
@@ -56,7 +62,29 @@ public class TerritoriaServiceImpl implements TerritorialService{
 
                 if (element.getAttribute("name").equals("NAZWA"))
                     nazwa = element.getTextContent();
+
             }
+
+            boolean isGmi = woj!=null && pow !=null && gmi!=null;
+            boolean isPow = woj!=null && pow!=null && !isGmi;
+            boolean isWoj = woj!=null && !isPow;
+            if (isGmi)
+            {
+
+            }
+            if (isPow)
+            {
+
+            }
+
+            if (isWoj)
+            {
+                if (!provinceList.stream().filter(a->a.getCode().equals("")).findFirst().isPresent())
+                {
+                    provinceList.add(new Province(woj,nazwa));
+                }
+            }
+
         }
 
     }
